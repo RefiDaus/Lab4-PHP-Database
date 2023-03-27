5a34cf71e0cc689d688a0a81708fc9586e8d19bb
RefiDaus <92767593+RefiDaus@users.noreply.github.com>
2023-03-27 09:11:45 +0700

    Add files via upload

---
+++ lab4_PHP_database/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "Refidaus123";
+$db = "latihan1";
+$conn = mysqli_connect($host, $user, $pass, $db);
+if ($conn == false)
+{
+echo "Koneksi ke server gagal.";
+die();
+} #else echo "Koneksi berhasil";
+?>
